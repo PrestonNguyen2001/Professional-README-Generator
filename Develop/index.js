9d79d00dd51fd673f37bb06b6760a2f6203015a1
PrestonNguyen2001 <prestonnguyen2001@gmail.com>
2024-03-26 12:46:21 -0400

    update index.js

--- Develop/index.js
+++ Develop/index.js
@@ -7,9 +7,121 @@ const questions = [
     // Add Questions 
     {
         type: 'input',
+        message: "What is the title of your project?",
         name: 'title',
-        message: 'What is the title of your project?',
+        validate: function (answer) {
+            if (answer.length < 1) {
+                return console.log("A valid project title is required.");
+            }
+            return true;
+        }
     },
+    {
+        type: 'input',
+        message: "Provide a description of your project:",
+        name: 'description',
+        validate: function (answer) {
+            if (answer.length < 1) {
+                return console.log("A valid project description is required.");
+            }
+            return true;
+        }
+    },
+    {
+        type: 'input',
+        message: "Provide installation instructions:",
+        name: 'installation',
+        validate: function (answer) {
+            if (answer.length < 1) {
+                return console.log("Please provide installation instructions.");
+            }
+            return true;
+        }
+    },
+    {
+        type: 'input',
+        message: "Provide usage information:",
+        name: 'usage',
+        validate: function (answer) {
+            if (answer.length < 1) {
+                return console.log("Please provide usage information.");
+            }
+            return true;
+        }
+    },
+    {
+        type: 'list',
+        message: "Choose a license for your application:",
+        name: 'license',
+        choices: ['MIT', 'GNU General Public License (GPL)', 'Apache License', 'BSD License', 'GNU Lesser General Public License (LGPL)', 'None'],
+        default: 'None',
+        validate: function (answer) {
+            if (answer === 'None') {
+                return true; 
+            }
+            if (answer.length < 1) {
+                return console.log("Please choose a license.");
+            }
+            return true;
+        }
+    },
+    {
+        type: 'input',
+        message: "What are the contribution guidelines?",
+        name: 'contributing',
+        validate: function (answer) {
+            if (answer.length < 1) {
+                return console.log("Please provide contribution guidelines.");
+            }
+            return true;
+        }
+    },
+    {
+        type: 'input',
+        message: "Provide test instructions:",
+        name: 'tests',
+        validate: function (answer) {
+            if (answer.length < 1) {
+                return console.log("Please provide test instructions.");
+            }
+            return true;
+        }
+    },
+    {
+        type: 'input',
+        message: "What is your GitHub username? (No @ needed)",
+        name: 'username',
+        default: 'connietran-dev',
+        validate: function (answer) {
+            if (answer.length < 1) {
+                return console.log("A valid GitHub username is required.");
+            }
+            return true;
+        }
+    },
+    {
+        type: 'input',
+        message: "What is the name of your GitHub repo?",
+        name: 'repo',
+        default: 'readme-generator',
+        validate: function (answer) {
+            if (answer.length < 1) {
+                return console.log("A valid GitHub repo is required for a badge.");
+            }
+            return true;
+        }
+    },
+    {
+        type: 'input',
+        message: "What is your email address?",
+        name: 'email',
+        validate: function (answer) {
+            if (answer.length < 1) {
+                return console.log("A valid email address is required.");
+            }
+            return true;
+        }
+    }
 ];
 
 // TODO: Create a function to write README file
